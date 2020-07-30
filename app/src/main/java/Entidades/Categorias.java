dcbd5bce4ded0a16eb9b83900c1a09d6484b96cd
Luis Guananga Gamarra <luisgamarra_97@hotmail.com>
2020-07-30 16:44:40 -0500

    Add de la entidad categoria

---
+++ app/src/main/java/Entidades/Categorias.java
@@ -0,0 +1,22 @@
+package Entidades;
+
+public class Categorias {
+    public static final String TABLE_NAME ="categorias";
+    public static final String ID ="id_categoria";
+    public static final String NOMBRE_CATEGORIA ="nombre_categoria";
+    public static final String ST_CATEGORIA ="st_categoria";
+
+    public static String creacionString(){
+        String sql = "CREATE TABLE "
+                +   TABLE_NAME+" ("
+                +   ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+                +   NOMBRE_CATEGORIA+" TEXT, "
+                +   ST_CATEGORIA+" TEXT, "
+                + "UNIQUE (" + Perfil.ID + "))";
+        return sql;
+    }
+    public static String dropString(){
+        String sql = "DROP TABLE IF EXISTS " +TABLE_NAME;
+        return sql;
+    }
+}
